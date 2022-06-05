772effe9e834adaefb8c06b77c999a2fc74eca3c
Leonardo Moraes <leonardo.moraes@westwing.com.br>
2022-06-05 10:28:45 -0300

    Criando model de partidas

---
+++ app/Models/SudokuMatch.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SudokuMatch extends Model
+{
+    use HasFactory;
+
+    protected $table = 'sudoku_matches';
+    
+}
